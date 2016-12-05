f025f40d5390678a27b12670e975a08dc564a08e
jujujujujuju1004 <jujujujajaja1004@gmail.com>
2016-12-06 08:00:12 +0900

    Dynamic Routes

--- app.js
+++ app.js
@@ -1,23 +1,32 @@
 var express = require('express');
 var app = express();
 
+var blocks = {
+  'Fixed':'Fastened securely in position',
+  'Movable':'Capable of being moved',
+  'Rotaling':'Moving in a circle around its center'
+};
+
 var logger = require('./logger');
 app.use(logger);
 
 app.use(express.static('public'));
 
-/*app.get('/', function(request, response) {
-  response.send('Hello world');
-  response.end();
-});*/
+app.get('/blocks/', function(request, response) {
+  if (request.query.limit >= 0) {
+    response.json(block.slice(0, request.query.limit));
+  } else {
+    response.json(blocks);
+  }
+});
 
-/*app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/public/index.html');
-});*/
-
-app.get('/blocks', function(request, response) {
-  var blocks = ['Fixed', 'Movable', 'Rotaling'];
-  response.json(blocks);
+app.get('/blocks/:name', function(request, response) {
+  var description = blocks[request.params.name];
+  if(!description) {
+    response.status(404).json('No description found for Banana');
+  } else {
+    response.json(description);
+  }
 });
 
 app.listen(3000, function() {
