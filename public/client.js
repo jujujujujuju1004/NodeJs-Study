ecab9584e6cdc6d1973c40815e032320cf7a9e9c
jujujujujuju1004 <jujujujajaja1004@gmail.com>
2016-12-09 14:18:19 +0900

    Body-Parser

--- public/client.js
+++ public/client.js
@@ -4,8 +4,23 @@ $(function(){
   function appendToList(blocks) {
     var list =[];
     for(var i in blocks) {
-      list.push($('<li>', { text: blocks[i] }));
+      block = blocks[i];
+      content = '<a href="blocks"/'+block+'">'+block+'</a>';
+      list.push($('<li>', { html: content }));
     }
     $('.block-list').append(list);
   }
+
+  $('form').on('submit', function(event) {
+    event.preventDefault();
+    var form = $(this);
+    var blcokData = form.serialize();
+
+    $.ajax({
+      type:'POST', url:'/blcok', data: blockData
+    }).done(function.(blockName) {
+      appendToList([blockName]);
+      form.trigger('reset');
+    });
+  });
 });
